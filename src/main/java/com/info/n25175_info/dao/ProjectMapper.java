57a08c7aac70274e88d30292b115deae7e95319a
jaiei <1016999820@qq.com>
2020-03-30 21:27:20 +0800

    search添加

--- src/main/java/com/info/n25175_info/dao/ProjectMapper.java
+++ src/main/java/com/info/n25175_info/dao/ProjectMapper.java
@@ -3,9 +3,12 @@ package com.info.n25175_info.dao;
 import com.info.n25175_info.MyMapper;
 import com.info.n25175_info.dto.project.ProjectDto;
 import com.info.n25175_info.entity.Project;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProjectMapper extends MyMapper<Project> {
 
     ProjectDto newByAid(Integer aId);
@@ -17,4 +20,7 @@ public interface ProjectMapper extends MyMapper<Project> {
     List<ProjectDto> findAll();
 
     ProjectDto findByPCode(String pCode);
+
+    //添加新函数用来获取搜索条件
+    List<ProjectDto> selectBypNamepCode(@Param("aId") Integer aId, @Param("pName") String pName,@Param("pCode") String pCode);
 }
